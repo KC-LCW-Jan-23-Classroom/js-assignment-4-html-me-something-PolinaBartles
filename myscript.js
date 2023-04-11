94d2a3ebbea824b9ab74f331b2244a6e94a26832
Polina Bartles <polina.bartles@gmail.com>
2023-04-10 22:56:58 -0500

    Added id and classes for paragraph

--- myscript.js
+++ myscript.js
@@ -1,7 +1,8 @@
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 
-toggleButton.addEventListener('click', function() {
+toggleButton.addEventListener('click', function(event) {
+  event.preventDefault();
   if (body.classList.contains('light-mode')) {
     body.classList.remove('light-mode');
     body.classList.add('dark-mode');
